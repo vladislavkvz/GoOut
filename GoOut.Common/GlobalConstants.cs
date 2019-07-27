5590ccf65db1dfe19b74925927a8df8d3f423fe6
Vladislav Kavazov <vladislav.kavazov@gmail.com>
2019-07-28 00:19:48 +0300

    Fix some FE part

--- GoOut.Common/GlobalConstants.cs
+++ GoOut.Common/GlobalConstants.cs
@@ -12,5 +12,9 @@
         public const string AdminUsername = "Administrator";
         public const string AdminFirstName = "Vladislav";
         public const string AdminLastName = "Kavazov";
+
+        // Cookie Consent and Privacy Page
+        public const string CookieConsent = "Use this space to summarize your privacy and cookie use policy";
+        public const string PrivacyPage = "Use this page to detail your site's privacy policy.";
     }
 }
